4f5df0039fe421c598ddeceb9507744184604cd9
hovitya <hovitya@gmail.com>
2014-09-29 18:06:21 +0200

    Add placeholder text

--- src/js/directives/imxInput.js
+++ src/js/directives/imxInput.js
@@ -1,26 +1,51 @@
-angular.module('imx.Input').directive('imxInput', [function () {
+angular.module('imx.Input').directive('imxInput', ['$log', function ($log) {
     return {
-        scope: {},
-        require: 'ngModel',
+        scope: {
+            placeholder: '@',
+            label: '@'
+        },
         restrict: 'E',
         transclude: true,
         replace: true,
         templateUrl: function(elem,attrs) {
             return attrs.templateUrl || 'template/partials/input.html';
         },
-        link: function (scope, iElement, iAttrs, ngModelController) {
-            ngModelController.$render = function () {
-                scope.value = ngModelController.$viewValue;
-            };
-
-            function updateModel(data) {
-                ngModelController.$setViewValue(data);
-                ngModelController.$render();
+        link: function (scope, iElement, iAttrs) {
+            scope.focus = false;
+            scope.empty = true;
+            //Look up ngModel
+            var inputs = iElement.find('input');
+            if (inputs.length > 1) {
+                $log.error("More than one input elements in one imxInput.");
+                return;
+            }
+            if (inputs.length === 0) {
+                $log.error("No input element in imxInput.");
+                return;
             }
 
-            scope.$watch('value', function (newValue) {
-                updateModel(newValue);
+            inputs.bind('focus', function () {
+                scope.focus = true;
+                scope.$digest();
             });
+
+            inputs.bind('blur', function () {
+                scope.focus = false;
+                if (inputs.val().length === 0) {
+                    scope.empty = true;
+                } else {
+                    scope.empty = false;
+                }
+                scope.$digest();
+            });
+
+            var model = inputs.controller('ngModel');
+            if (model) {
+                scope.model = model;
+            } else {
+                $log.warn('Input model not found');
+            }
+
         }
     };
 }]);
