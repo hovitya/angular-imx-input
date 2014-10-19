40767f6e8eee7b41bf0d71095f5ef6ecd825d51e
Viktor Horváth <hovitya@outlook.com>
2014-10-19 20:01:20 +0200

    Rename imxInputText to imxInput

--- src/js/directives/imxSlider.js
+++ src/js/directives/imxSlider.js
@@ -1,15 +1,27 @@
 angular.module('imx.Input').directive('imxSlider', ['$log', '$rootScope', function ($log, $rootScope) {
     function postLink(scope, iElement, iAttrs, ngModel) {
         inputBase(scope, iElement, iAttrs, ngModel);
+        ngModel.$render = function() {
+            if (ngModel.$viewValue === null || ngModel.$viewValue === undefined) {
+                ngModel.$setViewValue(0);
+                scope.data.value = 0;
+            }
+        };
     }
 
 
     return {
         restrict: 'E',
+        transclude: true,
         scope: {
-            label: '@'
+            label: '@',
+            min: '@',
+            max: '@'
         },
-        require: '?ngModel',
+        templateUrl: function(elem,attrs) {
+            return attrs.templateUrl || 'template/partials/inputSlider.html';
+        },
+        require: 'ngModel',
         link: postLink
     };
 }]);
